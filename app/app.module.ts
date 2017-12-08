b96d600824ce284469c0d74ba26f99fd9f389ae3
Admin <soderstromshane@gmail.com>
2017-12-08 15:07:36 -0700

    implemented angular form for new proposal

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { HomepageComponent } from './homepage/homepage.component';
 import { AppComponent } from './app.component';
 import { DocumentsComponent } from './documents/documents.component';
@@ -10,7 +11,8 @@ import { AppRoutingModule } from './app-routing.module';
 @NgModule({
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   declarations: [
     AppComponent,
